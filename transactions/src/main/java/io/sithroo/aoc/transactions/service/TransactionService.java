d2dde3fdb054bdc3612fd0ade0a31c41e83e4789
Pradeep Peiris <pradeep.peiris@hivestreaming.com>
2019-09-30 09:57:05 +0200

    Improve java docs

--- transactions/src/main/java/io/sithroo/aoc/transactions/service/TransactionService.java
+++ transactions/src/main/java/io/sithroo/aoc/transactions/service/TransactionService.java
@@ -4,10 +4,23 @@ import io.sithroo.aoc.transactions.domain.Transaction;
 
 import java.util.List;
 
+/**
+ * Application Service Layer for Transactions
+ */
 public interface TransactionService {
 
+    /**
+     * Create new Transaction
+     * @param transaction Transaction to be created
+     * @return created Transaction
+     */
     Transaction createTransaction(Transaction transaction);
 
+    /**
+     * Find transactions by AccountId
+     * @param accountId Account Id for transactions
+     * @return List of Transactions for given account id
+     */
     List<Transaction> getTransactions(String accountId);
 }
 
